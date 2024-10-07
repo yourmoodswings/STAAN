b2042f796d54a57309af9776d253828e917c7b83
yourmoodswings <138708654+yourmoodswings@users.noreply.github.com>
2024-10-07 21:06:25 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
+// config/db.js
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'staan',
-    password: '@StayAwake1',  // Replace with the password you set during PostgreSQL installation
-    port: 5432,
+  connectionString: process.env.DATABASE_URL, // Use DATABASE_URL from env
+  ssl: {
+    rejectUnauthorized: false, // Required for Render's managed PostgreSQL
+  },
 });
 
 client.connect()
-    .then(() => console.log('Connected to PostgreSQL'))
-    .catch(err => console.error('Connection error', err.stack));
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
 
 module.exports = client;
